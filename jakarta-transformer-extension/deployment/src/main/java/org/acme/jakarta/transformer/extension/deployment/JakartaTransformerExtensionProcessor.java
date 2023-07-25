21e42ff24c66a7cabf3430fc4bba53ecd869becb
DSkoufis <skoufisdimitris@gmail.com>
2023-07-25 08:48:32 +0100

    Solution for https://stackoverflow.com/q/76738232

---
+++ jakarta-transformer-extension/deployment/src/main/java/org/acme/jakarta/transformer/extension/deployment/JakartaTransformerExtensionProcessor.java
@@ -0,0 +1,14 @@
+package org.acme.jakarta.transformer.extension.deployment;
+
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.FeatureBuildItem;
+
+class JakartaTransformerExtensionProcessor {
+
+    private static final String FEATURE = "jakarta-transformer-extension";
+
+    @BuildStep
+    FeatureBuildItem feature() {
+        return new FeatureBuildItem(FEATURE);
+    }
+}
